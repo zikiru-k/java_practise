cb99bb6d3482915f0d42ae36e124e1469456df1a
zikiru-k <zikiru.1123@gmail.com>
2024-04-14 00:02:03 +0900

    java practise

---
+++ src/App.java
@@ -0,0 +1,18 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        System.out.println(5 + 3);
+        System.out.println("5 + 3");
+
+        int i = 5;
+        int j = 10;
+        System.out.println(i + j);
+
+        String m1 = "人間って";
+        String m2 = "いいな";
+        System.out.println(m1 + m2);
+
+        float f = 11;
+        System.out.println(f);
+    }
+}
