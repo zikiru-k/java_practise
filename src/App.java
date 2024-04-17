8722fbe7e1796fe7a5342da6b7a74064ffdf88c6
zikiru-k <zikiru.1123@gmail.com>
2024-04-17 23:22:42 +0900

    配列

--- src/App.java
+++ src/App.java
@@ -107,5 +107,22 @@ public class App {
             System.out.println("こんにちは");
         }
 
+        for(int i_1 = 1;i_1 < 10;i_1++){
+            for(int i_2 = 1;i_2 < 10; i_2++){
+                System.out.println(i_1 * i_2);
+            }
+        }
+
+        int sansu = 20;
+        int kokugo = 30;
+        int rika = 40;
+        int eigo = 50;
+        int syakai = 80;
+    
+        int sum = sansu + kokugo + rika + syakai + eigo;
+    
+        int avg = sum / 5;
+        System.out.println("合計点:" + sum);
+        System.out.println("平均点:" + avg);
     }
 }
