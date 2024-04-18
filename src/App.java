2e2e65c23fd3764e428c692e7d53eb81cd410c1b
zikiru-k <zikiru.1123@gmail.com>
2024-04-18 23:53:46 +0900

    配列

--- src/App.java
+++ src/App.java
@@ -1,69 +1,69 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        System.out.println(5 + 3);
-        System.out.println("5 + 3");
+        // System.out.println("Hello, World!");
+        // System.out.println(5 + 3);
+        // System.out.println("5 + 3");
 
-        int i = 5;
-        int j = 10;
-        System.out.println(i + j);
+        // int i = 5;
+        // int j = 10;
+        // System.out.println(i + j);
 
-        String m1 = "人間って";
-        String m2 = "いいな";
-        System.out.println(m1 + m2);
+        // String m1 = "人間って";
+        // String m2 = "いいな";
+        // System.out.println(m1 + m2);
 
-        float f = 11;
-        System.out.println(f);
+        // float f = 11;
+        // System.out.println(f);
 
-        char zodiac = '辰';
-        String name = "すがわら";
-        System.out.println(zodiac);
-        System.out.println(name);
+        // char zodiac = '辰';
+        // String name = "すがわら";
+        // System.out.println(zodiac);
+        // System.out.println(name);
 
-        int age = 20;
-        System.out.println("私の年齢は、" + age + "歳です。");
-        age = 35;
-        System.out.println("サバを読みました。実は、" + age + "歳です。");
+        // int age = 20;
+        // System.out.println("私の年齢は、" + age + "歳です。");
+        // age = 35;
+        // System.out.println("サバを読みました。実は、" + age + "歳です。");
 
-        final double pi = 3.14;
-        int pie = 5;
-        System.out.println("半径" + pie + "cmの体積は、");
-        System.out.println(pie * pie * pi);
+        // final double pi = 3.14;
+        // int pie = 5;
+        // System.out.println("半径" + pie + "cmの体積は、");
+        // System.out.println(pie * pie * pi);
 
-        String prof1 = "名前：ナマエタロウ\n(なまえたろう)";
-        String prof2 = """
-                        名前：二ホンハナコ
-                        (にほんはなこ)
-                        """;
-        System.out.println(prof1);
-        System.out.println(prof2);
+        // String prof1 = "名前：ナマエタロウ\n(なまえたろう)";
+        // String prof2 = """
+        //                 名前：二ホンハナコ
+        //                 (にほんはなこ)
+        //                 """;
+        // System.out.println(prof1);
+        // System.out.println(prof2);
 
-        int a = 10;
-        int b = 10;
-        System.out.println(a++ + 50);
-        System.out.println(++b + 50);
+        // int a = 10;
+        // int b = 10;
+        // System.out.println(a++ + 50);
+        // System.out.println(++b + 50);
 
-        int age2 = (int)5.5;
-        System.out.println(age2);
+        // int age2 = (int)5.5;
+        // System.out.println(age2);
 
-        int a2 = 15;
-        int b2 = 20;
-        int m = Math.max(a2, b2);
-        System.out.println("比較実験：" + a2 + "と" + b2 + "で大きい方は…" + m + "です");
-        int n = Math.min(a2, b2);
-        System.out.println("比較実験：" + a2 + "と" + b2 + "で小さい方は…" + n + "です");
+        // int a2 = 15;
+        // int b2 = 20;
+        // int m = Math.max(a2, b2);
+        // System.out.println("比較実験：" + a2 + "と" + b2 + "で大きい方は…" + m + "です");
+        // int n = Math.min(a2, b2);
+        // System.out.println("比較実験：" + a2 + "と" + b2 + "で小さい方は…" + n + "です");
 
-        String Stringage = "20";
-        int Stringage2 =Integer.parseInt(Stringage);
-        System.out.println((Stringage2 + 1) + "歳です");
+        // String Stringage = "20";
+        // int Stringage2 =Integer.parseInt(Stringage);
+        // System.out.println((Stringage2 + 1) + "歳です");
 
-        int r = new java.util.Random().nextInt(10);
-        System.out.println("選ばれた数は" + r + "でした");
+        // int r = new java.util.Random().nextInt(10);
+        // System.out.println("選ばれた数は" + r + "でした");
 
-        // String Sinput = new java.util.Scanner(System.in).nextLine();
-        // System.out.println(Sinput);
-        // int Iinput = new java.util.Scanner(System.in).nextInt();
-        // System.out.println(Iinput);
+        // // String Sinput = new java.util.Scanner(System.in).nextLine();
+        // // System.out.println(Sinput);
+        // // int Iinput = new java.util.Scanner(System.in).nextInt();
+        // // System.out.println(Iinput);
 
         boolean tenki = true;
         if (tenki == true) {
@@ -124,5 +124,37 @@ public class App {
         int avg = sum / 5;
         System.out.println("合計点:" + sum);
         System.out.println("平均点:" + avg);
+
+        int[] scores = new int[5];
+        int num = scores.length;
+        System.out.println("要素の数: " + num);
+
+        scores[1] = 30;
+        System.out.println(scores[1]);
+
+        int[] seq = new int[10];
+
+        // 塩基配列をランダムに生成
+        for (int i = 0; i < seq.length; i++) {
+          seq[i] = new java.util.Random().nextInt(4);
+        }
+    
+        // 生成した塩基配列の記号を表示
+        for (int i = 0; i < seq.length; i++) {
+          switch (seq[i]) {
+            case 0 -> {
+              System.out.print("A ");
+            }
+            case 1 -> {
+              System.out.print("T ");
+            }
+            case 2 -> {
+              System.out.print("G ");
+            }
+            case 3 -> {
+              System.out.print("C ");
+            }
+          }
+        }
     }
 }
