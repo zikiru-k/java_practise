74095bc7b79f61d622e7a81f7493ed46f64a5207
zikiru-k <zikiru.1123@gmail.com>
2024-04-20 23:53:16 +0900

    メソッド呼び出し練習

--- src/App.java
+++ src/App.java
@@ -65,103 +65,117 @@ public class App {
         // // int Iinput = new java.util.Scanner(System.in).nextInt();
         // // System.out.println(Iinput);
 
-        boolean tenki = true;
-        if (tenki == true) {
-            System.out.println("洗濯する");
-        } else {
-            System.out.println("洗濯無理");
-        }
+        // boolean tenki = true;
+        // if (tenki == true) {
+        //     System.out.println("洗濯する");
+        // } else {
+        //     System.out.println("洗濯無理");
+        // }
         
-        String str = "すっきり";
-        if(str.equals("すっきり")){
-            System.out.println("同じ");
-        }
+        // String str = "すっきり";
+        // if(str.equals("すっきり")){
+        //     System.out.println("同じ");
+        // }
 
-        System.out.println("あなたの運勢を占います");
-        int fortune = new java.util.Random().nextInt(4) + 1;
-        if (fortune == 1) {
-          System.out.println("大吉");
-        } else if (fortune == 2) {
-          System.out.println("中吉");
-        } else if (fortune == 3) {
-          System.out.println("吉");
-        } else {
-          System.out.println("凶");
-        }
+        // System.out.println("あなたの運勢を占います");
+        // int fortune = new java.util.Random().nextInt(4) + 1;
+        // if (fortune == 1) {
+        //   System.out.println("大吉");
+        // } else if (fortune == 2) {
+        //   System.out.println("中吉");
+        // } else if (fortune == 3) {
+        //   System.out.println("吉");
+        // } else {
+        //   System.out.println("凶");
+        // }
 
-        System.out.println("あなたの運勢を占います");
-        int fortune2 = new java.util.Random().nextInt(5) + 1;
-        switch (fortune2) {
-          case 1, 2 -> {
-            System.out.println("いいね！");
-          }
-          case 3 -> {
-            System.out.println("普通です");
-          }
-          case 4, 5 -> {
-            System.out.println("うーん...");
-          }
-        }
+        // System.out.println("あなたの運勢を占います");
+        // int fortune2 = new java.util.Random().nextInt(5) + 1;
+        // switch (fortune2) {
+        //   case 1, 2 -> {
+        //     System.out.println("いいね！");
+        //   }
+        //   case 3 -> {
+        //     System.out.println("普通です");
+        //   }
+        //   case 4, 5 -> {
+        //     System.out.println("うーん...");
+        //   }
+        // }
 
-        for(int q = 0; q <= 5; q++){
-            System.out.println("こんにちは");
-        }
+        // for(int q = 0; q <= 5; q++){
+        //     System.out.println("こんにちは");
+        // }
 
-        for(int i_1 = 1;i_1 < 10;i_1++){
-            for(int i_2 = 1;i_2 < 10; i_2++){
-                System.out.println(i_1 * i_2);
-            }
-        }
+        // for(int i_1 = 1;i_1 < 10;i_1++){
+        //     for(int i_2 = 1;i_2 < 10; i_2++){
+        //         System.out.println(i_1 * i_2);
+        //     }
+        // }
 
-        int sansu = 20;
-        int kokugo = 30;
-        int rika = 40;
-        int eigo = 50;
-        int syakai = 80;
+        // int sansu = 20;
+        // int kokugo = 30;
+        // int rika = 40;
+        // int eigo = 50;
+        // int syakai = 80;
     
-        int sum = sansu + kokugo + rika + syakai + eigo;
+        // int sum = sansu + kokugo + rika + syakai + eigo;
     
-        int avg = sum / 5;
-        System.out.println("合計点:" + sum);
-        System.out.println("平均点:" + avg);
+        // int avg = sum / 5;
+        // System.out.println("合計点:" + sum);
+        // System.out.println("平均点:" + avg);
 
-        int[] scores = new int[5];
-        int num = scores.length;
-        System.out.println("要素の数: " + num);
+        // int[] scores = new int[5];
+        // int num = scores.length;
+        // System.out.println("要素の数: " + num);
 
-        scores[1] = 30;
-        System.out.println(scores[1]);
+        // scores[1] = 30;
+        // System.out.println(scores[1]);
 
-        int[] seq = new int[10];
-        for (int i = 0; i < seq.length; i++) {
-          seq[i] = new java.util.Random().nextInt(4);
-        }
-        for (int i = 0; i < seq.length; i++) {
-          switch (seq[i]) {
-            case 0 -> {
-              System.out.print("A ");
-            }
-            case 1 -> {
-              System.out.print("T ");
-            }
-            case 2 -> {
-              System.out.print("G ");
-            }
-            case 3 -> {
-              System.out.print("C ");
-            }
-          }
-        }
+        // int[] seq = new int[10];
+        // for (int i = 0; i < seq.length; i++) {
+        //   seq[i] = new java.util.Random().nextInt(4);
+        // }
+        // for (int i = 0; i < seq.length; i++) {
+        //   switch (seq[i]) {
+        //     case 0 -> {
+        //       System.out.print("A ");
+        //     }
+        //     case 1 -> {
+        //       System.out.print("T ");
+        //     }
+        //     case 2 -> {
+        //       System.out.print("G ");
+        //     }
+        //     case 3 -> {
+        //       System.out.print("C ");
+        //     }
+        //   }
+        // }
 
-        int[][] scores2 = {{40, 50, 60}, {80, 60, 70}};
-        System.out.println(scores2.length);
-        System.out.println(scores2[0].length);
+        // int[][] scores2 = {{40, 50, 60}, {80, 60, 70}};
+        // System.out.println(scores2.length);
+        // System.out.println(scores2[0].length);
         
-        System.out.println("メソッドを呼び出します");
-        hello();
-        System.out.println("メソッドの呼び出しが終わりました");
+        // System.out.println("メソッドを呼び出します");
+        // hello();
+        // System.out.println("メソッドの呼び出しが終わりました");
+
+        introduceOneself();
+
       }
-      public static void hello() {
-        System.out.println("湊さん、こんにちは");
+      // public static void hello() {
+      //   System.out.println("湊さん、こんにちは");
+      // }
+
+      public static void introduceOneself(){
+        String name = "湊雄輔";
+        int age = 22;
+        double height = 169.9;
+        char zodiac = '辰';
+        System.out.println("私の名前は" + name + "です");
+        System.out.println("歳は" + age + "です");
+        System.out.println("身長は" + height + "cmです");
+        System.out.println("十二支は" + zodiac + "です");
       }
 }
