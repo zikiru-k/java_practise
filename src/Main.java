0e4b6f3e1cb63aeb5f1b433879eed4ccde3bb46c
zikiru-k <zikiru.1123@gmail.com>
2024-04-28 00:00:16 +0900

    インスタンスの利用

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,25 @@ public class Main {
         Hero h = new Hero();
         h.name = "トオル";
         h.hp = 100;
-        System.out.println("勇者" + h.name + "を生み出しました！");
+        // System.out.println("勇者" + h.name + "を生み出しました！");
+
+        // h.sit(5);
+        // h.slip();
+        // h.sit(25);
+        // h.run();
+
+        Matango m1 = new Matango();
+        m1.hp = 50;
+        m1.suffix = 'A';
+    
+        Matango m2 = new Matango();
+        m2.hp = 48;
+        m2.suffix = 'B';
+    
+        // 冒険のはじまり
+        h.slip();
+        m1.run();
+        m2.run();
+        h.run();
       }
 }
